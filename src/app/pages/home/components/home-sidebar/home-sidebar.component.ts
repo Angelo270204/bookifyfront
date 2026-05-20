e1cc37eb6aaf61e2c6a043d34027801df8a6be84
yerico <u21230122@utp.edu.pe>
2026-05-20 18:38:12 -0500

    barra actualizada

--- src/app/pages/home/components/home-sidebar/home-sidebar.component.ts
+++ src/app/pages/home/components/home-sidebar/home-sidebar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './home-sidebar.component.html',
   styleUrl: './home-sidebar.component.scss'
 })
